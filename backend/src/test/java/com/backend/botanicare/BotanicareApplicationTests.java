94045209732df0a4b87579d6be57f8d57be4c875
Jose Chacin <jose.chacinn21@gmail.com>
2025-04-22 17:45:55 +0200

    40 - Einrichtung der SpringBoot Anwendung mittels Spring Initializr

---
+++ backend/src/test/java/com/backend/botanicare/BotanicareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.botanicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotanicareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
