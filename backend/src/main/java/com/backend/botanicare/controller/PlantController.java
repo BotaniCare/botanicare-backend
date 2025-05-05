3120a86cd56a068f34af90fcbf92b5e8f8a2cca5
Jose Chacin <jose.chacinn21@gmail.com>
2025-05-05 15:21:48 +0200

    Generierte API Endpunkte im Controller implementieren.

--- backend/src/main/java/com/backend/botanicare/controller/PlantController.java
+++ backend/src/main/java/com/backend/botanicare/controller/PlantController.java
@@ -1,10 +1,44 @@
 package com.backend.botanicare.controller;
 
+import com.backend.botanicare.model.Plant;
+import com.backend.botanicare.service.PlantService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/plants")
 public class PlantController {
 
+    private final PlantService plantService;
 
+    public PlantController(PlantService plantService) {
+        this.plantService = plantService;
+    }
+
+    @GetMapping
+    public List<Plant> getAllPlants() {
+        return plantService.getAllPlants();
+    }
+
+    @GetMapping("/{id}")
+    public Plant getPlantById(@PathVariable Long id) {
+        return plantService.getPlantById(id);
+    }
+
+    @PostMapping
+    public Plant createPlant(@RequestBody Plant newPlant) {
+        return plantService.createPlant(newPlant);
+    }
+
+    @PutMapping("/{id}")
+    public Plant updatePlant(@PathVariable Long id, @RequestBody Plant updatedPlant) {
+        return plantService.updatePlant(id, updatedPlant);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePlant(@PathVariable Long id) {
+        plantService.deletePlant(id);
+    }
 }
