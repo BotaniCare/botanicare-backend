ca5f6f5de461e49330a2f26848e7e6b93afe7e25
mxw02 <maurice.woike@gmail.com>
2025-05-10 11:06:00 +0200

    Adds missing repository annotation

--- backend/src/main/java/com/backend/botanicare/repository/RoomRepository.java
+++ backend/src/main/java/com/backend/botanicare/repository/RoomRepository.java
@@ -2,6 +2,8 @@ package com.backend.botanicare.repository;
 
 import com.backend.botanicare.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room, String> {
 }
